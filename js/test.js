9900a651ff023839676e1f14544533bfcb2c8543
Adam Wallacessh-keygen -t rsa -b 4096 -C awallace689@gmail.com <awallace689@gmail.com>
2019-05-19 16:08:47 -0500

    Added bootstrap, draw random colored circle

--- js/test.js
+++ js/test.js
@@ -1,11 +1,62 @@
+"use strict";
+/*****************************
+ * Author: Adam Wallace
+ * Date: 5/18/2019
+ *****************************/
+let WIDTH = 600;
+let HEIGHT = 600;
+
+
 window.onload = () => {
-    getContext();
+    main();
 }
 
-function getContext() {
-    var c = document.getElementById('game');
-    console.log(c);
 
-    c.getContext('2d');
+/// Returns game canvas rendering object.
+/// 
+/// @return: CanvasRenderingContext2D
+function getContext() {
+    let canvas = document.getElementById('game');
+    let context = canvas.getContext('2d');
+    return context;
+}
+
+
+function main() {
+    let c = getContext();
+    let rad = 50;
+    c.canvas.width = WIDTH;
+    c.canvas.height = HEIGHT;
+    console.log(c);
+    drawRandomCircle(c, rad);
     
+}
+
+
+/// Draw circle at position ('x', 'y'), with radius 'rad'.
+/// 
+/// @param context {CanvasRenderingContext2D}: Canvas render context
+/// @param x {Number}: x-pos
+/// @param y {Number}: y-pos
+/// @param rad {Number}: radius
+/// @param color {}
+/// @return: undefined
+function drawCircle(context, x, y, rad, color) {
+    context.clearRect(0,0,WIDTH-1,HEIGHT-1);
+    context.beginPath();
+    context.moveTo(x, y);
+    context.arc(x, y, rad, 0, 2*Math.PI);
+    context.fillStyle = color ? color : 'black';
+    context.fill();
+}
+
+function drawRandomCircle(context, radius) {
+    drawCircle(context, 
+        Math.floor(Math.random() * ((WIDTH-radius)-radius) + radius),
+        Math.floor(Math.random() * ((HEIGHT-radius)-radius) + radius), 
+        radius, 
+        `rgb(
+            ${Math.floor(Math.random() * 255)},
+            ${Math.floor(Math.random() * 255)},
+            ${Math.floor(Math.random() * 255)})`);
 }
